6d57a78ae0a2c9ef39f472fc69cfe655d6ca27ed
UsuallyChris <schoonc@gmail.com>
2019-05-21 13:51:04 -0700

    Added base form structure and updated component state

--- src/components/PostForm.js
+++ src/components/PostForm.js
@@ -3,12 +3,30 @@ import React, { Component } from 'react';
 class PostForm extends Component {
   constructor(props) {
     super(props);
-    this.state = {}
+    this.state = {
+      title: '',
+      body: '',
+    }
   }
 
   render() {
     return (
-      <h1>Add Post</h1>
+      <div>
+        <h1>Add Post</h1>
+        <form action="">
+          <div>
+            <label htmlFor="">Title: </label><br />
+            <input type="text" name="title" id=""/>
+          </div>
+          <br/>
+          <div>
+            <label htmlFor="">Body: </label><br />
+            <textarea name="body"></textarea>
+          </div>
+          <br/>
+          <button type="submit">Submit</button>
+        </form>
+      </div>
     )
   }
 }
