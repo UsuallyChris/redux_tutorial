4b2a316d62063cc2169887446dec31bec3df910d
UsuallyChris <schoonc@gmail.com>
2019-05-21 13:36:51 -0700

    Added api call

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,6 +1,13 @@
 import React, { Component } from 'react';
 
 class Posts extends Component {
+
+  componentWillMount() {
+    fetch('https://jsonplaceholder.typicode.com/posts')
+      .then(res => res.json())
+      .then(data => console.log(data));
+  }
+
   render() {
     return (
       <h1>Posts</h1>
