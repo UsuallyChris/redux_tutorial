fdc545c6d2fdf918e8c1d2de43053fdf54784ea5
UsuallyChris <schoonc@gmail.com>
2019-05-21 15:50:44 -0700

    Added POST to form

--- src/components/PostForm.js
+++ src/components/PostForm.js
@@ -7,21 +7,47 @@ class PostForm extends Component {
       title: '',
       body: '',
     }
+
+    this.onChange = this.onChange.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+  }
+
+  onChange(e) {
+    this.setState({ [e.target.name]: e.target.value })
+  }
+
+  onSubmit(e) {
+    e.preventDefault();
+
+    const post = {
+      title: this.state.title,
+      body: this.state.body
+    }
+
+    fetch('https://jsonplaceholder.typicode.com/posts', {
+      method: 'POST',
+      headers: {
+        'content-type': 'application/json'
+      },
+      body: JSON.stringify(post)
+    })
+    .then(res => res.json())
+    .then(data => console.log(data))
   }
 
   render() {
     return (
       <div>
         <h1>Add Post</h1>
-        <form action="">
+        <form onSubmit={this.onSubmit}>
           <div>
             <label htmlFor="">Title: </label><br />
-            <input type="text" name="title" id=""/>
+            <input type="text" name="title" id="" onChange={this.onChange} value={this.state.title}/>
           </div>
           <br/>
           <div>
             <label htmlFor="">Body: </label><br />
-            <textarea name="body"></textarea>
+            <textarea name="body" onChange={this.onChange} value={this.state.body}></textarea>
           </div>
           <br/>
           <button type="submit">Submit</button>
