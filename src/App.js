32ce800f78ce2652b1836c71bcf25536b7c063be
UsuallyChris <schoonc@gmail.com>
2019-05-21 18:54:25 -0700

    Moved state to app level with redux

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import { Provider } from 'react-redux'
 import Posts from './components/Posts';
 import PostForm from './components/PostForm';
 
+import store from './store'
+
 function App() {
   return (
     <Provider store={store}>
